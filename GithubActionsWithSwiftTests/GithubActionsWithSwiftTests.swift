05f0d7259ec521d853c19993564c266209e5c091
Adriano Souza Costa <adriano.souza@consultaremedios.com.br>
2020-07-27 16:56:54 -0300

    Adicionando primeiro caso de teste

--- GithubActionsWithSwiftTests/GithubActionsWithSwiftTests.swift
+++ GithubActionsWithSwiftTests/GithubActionsWithSwiftTests.swift
@@ -7,28 +7,18 @@
 //
 
 import XCTest
+
 @testable import GithubActionsWithSwift
 
 class GithubActionsWithSwiftTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testSumTwoNumbers() throws {
+        let firstNumber = 10
+        let secondNumber = 13
+        let sum = firstNumber + secondNumber
+        let expectedResult = 23
+        
+        XCTAssertEqual(sum, expectedResult)
     }
 
 }

ccff2f6a137900aff0ae84e52d9b3232da965ff7
Adriano Souza Costa <adriano.souza@consultaremedios.com.br>
2020-07-27 16:51:17 -0300

    First commit

---
+++ GithubActionsWithSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubActionsWithSwift
+//
+//  Created by Adriano Souza Costa on 27/07/20.
+//  Copyright © 2020 Experiments. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
